9273841671fdb492e9c07e4d95994a346a59effd
Brujitari <natimontesg2@mail.com>
2022-02-26 22:45:23 +0100

    feat: ejercicios 20 y 21 hechos

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
+// 1
 let variableSinValor
+
+//2
 let booleano1 = true
 let booleano2 = true
+
+//3
 const PI = 3.14
+
+//4
 const TAU = PI * 2
+
+
+//5
 const booleanoAnd = booleano1 && booleano2
+
+//6
 const booleanoNot = !booleano1
+
+//7
 const booleanoMix0 = (booleano1 || booleano2) && (booleano1 || (!booleano1 && !booleano2))
 
 
+//8
 let incrementarDesp = 2
 let resultadoDesp = incrementarDesp++
+
+//9
 let incrementarAntes = 2
 let resultadoAntes = ++incrementarAntes
 
 
+//10
 let contarHasta10_2 = 0
 
 for (; contarHasta10_2 < 10; contarHasta10_2++) {
@@ -21,17 +39,16 @@ for (; contarHasta10_2 < 10; contarHasta10_2++) {
 }
 
 
+//11
 let postI = 0
 let postJ = 0
-
 
 for (let i = 0; i <= 10; i++) {
     postI += postJ++
 
 }
 
-
-
+//12
 let sumaPares = 0
 
 for (let i = 0; i < 10; i++) {
@@ -41,21 +58,31 @@ for (let i = 0; i < 10; i++) {
 }
 
 
-
+//13
 let variableValorNumerico = 1
 
+//14
 const MiNombre = 'Nat'
 
+//15
 const MiNumeroFav = 58734
 
+//16
 const booleanoOr = booleano1 || booleano2
 
+//17
 const booleanoMix1 = (booleano1 && (TAU/2 === PI) || (variableValorNumerico >= MiNumeroFav))
 
+//18
 const seisNoEsNueve = 6 !== 9
 
+//19
 const booleanoMix2 = variableValorNumerico > 0 || variableValorNumerico < -(MiNumeroFav * TAU)
 
+//20
+const valorSuma = MiNumeroFav + variableValorNumerico
 
+//21
+const valorResta = MiNumeroFav - variableValorNumerico
 
 
