4f00f8125a40cdff42b1b1796c9f8a08f6cdda9f
Brujitari <natimontesg2@mail.com>
2022-02-26 03:48:37 +0100

    feat: ejercicios 7,8,9 y 10

--- index.js
+++ index.js
@@ -4,4 +4,39 @@ let booleano2 = true
 const PI = 3.14
 const TAU = PI * 2
 const booleanoAnd = booleano1 && booleano2
-const booleanoNot =! booleano1
+const booleanoNot = !booleano1
+const booleanoMix0 = (booleano1 || booleano2) && (booleano1 || (!booleano1 && !booleano2))
+
+
+let incrementarDesp = 2
+let resultadoDesp = incrementarDesp++
+let incrementarAntes = 2
+let resultadoAntes = ++incrementarAntes
+
+
+let contarHasta10_2 = 0
+
+for (; contarHasta10_2 < 10; contarHasta10_2++) {
+
+}
+
+
+let postI = 0
+
+for (; postI < 11; postI++) {
+    ;
+
+}
+
+let postJ = 0
+
+for (; postJ < 11; postJ++) {
+    ;
+
+}
+
+
+
+
+
+
